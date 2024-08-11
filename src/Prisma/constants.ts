d8dabbb73c544957d028b94ebd7ab9b4b09dddfe
TkTnX <timur20-09@mail.ru>
2024-08-11 20:24:05 +0500

    добавлены новые категории

--- src/Prisma/constants.ts
+++ src/Prisma/constants.ts
@@ -1,5 +1,3 @@
-import { IProduct } from "@/@types/product";
-
 export const categories = [
   {
     name: "Роллы",
@@ -21,7 +19,6 @@ export const categories = [
     name: "Напитки",
     imageUrl: "/categories/05.svg",
   },
- 
 ];
 
 export const products = [
@@ -168,5 +165,148 @@ export const products = [
     price: 190, // Рандомное число
     weight: 235,
     categoryId: 2,
+  },
+  {
+    imageUrl: "/products/17.png",
+    name: "Сет #1",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 950,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/18.png",
+    name: "Сет #2",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 895,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/19.png",
+    name: "Сет #3",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 900,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/20.png",
+    name: "Сет Осака",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 252,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/21.png",
+    name: "Суши лосось в нори",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 1600,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/22.png",
+    name: "Сет #6",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 1400,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/23.png",
+    name: "Сет #7",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 1350,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/24.png",
+    name: "Гункан Сет",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100, // случайное число от 100 до 200
+    weight: 1040,
+    categoryId: 3,
+  },
+  {
+    imageUrl: "/products/25.png",
+    name: "Тартар из лосося с халапеньо",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 180,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/26.png",
+    name: "Тартар из лосося с авокадо",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 180,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/27.png",
+    name: "Салат Чука-Вакаме",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 185,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/28.png",
+    name: "Салат с киноа, авокадо и курицей",
+    description:
+      "Нори, рис, японский майонез, бальзамик, трюфельная сальса, кунжутное масло",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 252,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/29.png",
+    name: "Инаги с тунцом и трюфелем",
+    description:
+      "рис, японский лук, японский майонез, имбирь, трюфель, бальзамик",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 60,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/30.png",
+    name: "Инаги с морским гребешком и сыром",
+    description:
+      "рис, морской гребешок, японский майонез, имбирь, сыр, трюфель",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 60,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/31.png",
+    name: "Инаги с лососем и авокадо",
+    description: "рис, лосось, авокадо, манго, имбирь, арахис, пряный соус",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 60,
+    categoryId: 4,
+  },
+  {
+    imageUrl: "/products/32.png",
+    name: "Тартар микс",
+    description:
+      "лосось, тунец, морской гребешок, имбирь, японский лук, кунжут, чука-вакаме",
+    price: Math.floor(Math.random() * 100) + 100,
+    weight: 225,
+    categoryId: 4,
   },
 ];
