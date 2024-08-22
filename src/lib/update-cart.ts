b193f21e11532c3bc83767508f958464804a3b44
TkTnX <timur20-09@mail.ru>
2024-08-22 19:51:47 +0500

    totalPrice в корзине теперь добавляется в бд

--- src/lib/update-cart.ts
+++ src/lib/update-cart.ts
@@ -1,19 +1,31 @@
 import { prisma } from "@/Prisma/prisma-client";
+import { calcTotalPrice } from "./calc-cart-total-price";
 
 export const updateCart = async (token: string) => {
-    const userCart = await prisma.cart.findFirst({
-      where: { token },
-      include: {
-        cartItems: {
-          orderBy: {
-            quantity: "desc",
-          },
-          include: {
-            productItem: true,
-          },
+  const userCart = await prisma.cart.findFirst({
+    where: { token },
+    include: {
+      cartItems: {
+        orderBy: {
+          quantity: "desc",
+        },
+        include: {
+          productItem: true,
         },
       },
-    });
+    },
+  });
 
-    return userCart;
-}
+  const totalPrice = calcTotalPrice({ items: userCart });
+
+  await prisma.cart.update({
+    where: {
+      id: userCart?.id,
+    },
+    data: {
+      totalPrice,
+    },
+  });
+
+  return userCart;
+};
