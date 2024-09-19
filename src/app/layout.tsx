94d39278468d1be784654e24e607440f25892782
TkTnX <timur20-09@mail.ru>
2024-09-19 17:04:02 +0500

    проверка кеширования

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ninja Sushi",
 };
-
+export const fetchCache = "force-no-store";
 export default function AppLayout({
   children,
 }: Readonly<{
